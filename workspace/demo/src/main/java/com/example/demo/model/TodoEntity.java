1b6ec6be4099801bd6abd986b0e33b9209fcb9f9
20210699 <20210699@duksung.ac.kr>
2024-03-25 16:13:14 +0900

    0325

--- workspace/demo/src/main/java/com/example/demo/model/TodoEntity.java
+++ workspace/demo/src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,15 +1,22 @@
 package com.example.demo.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity //DB에 저장 가능
+@Table(name = "Todo")
 public class TodoEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String id;
     private String userId;
     private String title;
--- workspace/demo/src/main/java/com/example/demo/service/TodoService.java
+++ workspace/demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,12 +1,42 @@
 package com.example.demo.service;
 
+import com.example.demo.Repository.TodoRepository;
+import com.example.demo.model.TodoEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service //자바 빈
 public class TodoService {
+    @Autowired
+    private TodoRepository repository;
     public String testService(){
-        return "Test Service";
+        //TodoEntity 생성
+        TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+        //TodoEntity 저장
+        repository.save(entity);
+        //TodoEntity 검색
+        TodoEntity savedEntity = repository.findById(entity.getId()).get();
+        return savedEntity.getTitle();
     }
 
+    public List<TodoEntity> create(final TodoEntity entity) {
+        if (entity == null) {
+            log.warn("Entity cannot be null");
+            throw new RuntimeException("Entity cannot be null");
+        }
+
+        if (entity.getUserId() == null) {
+            log.warn("Unknown User");
+            throw new RuntimeException("Unknown User");
+        }
+        repository.save(entity);
+        log.info("Entity Id: {} is saved", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+    }
 
 }
