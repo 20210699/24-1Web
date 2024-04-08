96a1d1cdeac8dcd49daabbee87f1260a98a2f1ab
20210699 <20210699@duksung.ac.kr>
2024-04-08 16:14:50 +0900

    0408 react

--- workspace/react-workspace/todo-react-app/src/App.js
+++ workspace/react-workspace/todo-react-app/src/App.js
@@ -1,12 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 import Todo from './Todo';
+import React,{useState} from 'react';
 
 function App() {
+  const [items, setItem] = useState([{
+    id: "0",
+    title: "Hello World 1",
+    done: true
+  },
+  {
+    id: "1",
+    title: "Hello World 2",
+    done: false
+  },
+  {
+    id: "2",
+    title: "Hello World 3",
+    done: false
+  },
+  {
+    id: "3",
+    title: "Hello World 4",
+    done: false
+  },
+]);
+
+// JSX 결과를 변수에 저장
+let todoItems = 
+  items.length >0 && items.map((item)=> <Todo item={item} key={item.id}/>)
+
+// 변수를 반환
   return (
     <div className="App">
-      <Todo/>
-      <Todo/>
+      {/* <Todo item = {items[0]}/>
+      <Todo item = {items[1]}/>
+      <Todo item = {items[2]}/> */}
+      {todoItems}
     </div>
   );
 }
--- workspace/react-workspace/todo-react-app/src/Todo.js
+++ workspace/react-workspace/todo-react-app/src/Todo.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react"; //디폴트로 export한 경우, {} 생략/ export 하지 않은 것들은 {}
 
-const Todo = () => {
-    return(
-        <div className="Todo">
-            <input type="checkbox" id="todo0" name="todo0" value="todo0"/>
-            <label for="todo0">Todo 컴포넌트 만들기</label>
-        </div>
-    );
+const Todo = (props) => {
+    //let item = props.item;
+
+    const [item, setItem] = useState(props.item); // props.items : 상태 변수에 담길 초기값 / return 값이 배열
+
+    let output = <div className="Todo">
+                    
+                    <input 
+                        type="checkbox" 
+                        id={item.id} 
+                        name={item.id} 
+                        checked={item.done}
+                    />
+
+                        <label id={item.id}> {item.title}</label>
+                </div>;
+
+    return output;
 };
 
 export default Todo; //다른 컴포넌트가 이용 가능
