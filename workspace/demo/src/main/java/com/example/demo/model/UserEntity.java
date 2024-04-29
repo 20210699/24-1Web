7a0422876d376007bebb69e81cddebc96d8c601c
20210699 <20210699@duksung.ac.kr>
2024-04-29 16:14:08 +0900

    0429 auth

--- workspace/demo/src/main/java/com/example/demo/model/UserEntity.java
+++ workspace/demo/src/main/java/com/example/demo/model/UserEntity.java
@@ -1,4 +1,24 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "username")})
 public class UserEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+    @Column
+    private String username;
+    private String password;
+    private String role;
+    private String authProvider;
 }
